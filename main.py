c90455f72b5433d926feb8a0104c8a6e16fb8215
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-03-08 16:14:00 +0300

    fully functional converter.

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from tkinter import *
 
 window = Tk()
-window.title("Units Converter Program")
-
 window.minsize(width=500, height=300)
+window.title("Mile to Km Converter")
+window.config(pady=100, padx=150)
 
-label = Label(text="Enter miles here")
-label.grid(column=0, row=0)
+miles_input = Entry()
+miles_input.grid(column=1, row=0)
 
-input = Entry(width=10)
-input.grid(column=3, row=2)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+km_value = Label(text="0")
+km_value.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
 
 
-def converter():
-    kilometer = round(int(input.get()) * 1.609, 2)
-    print(f"{kilometer}km")
-    label.config(text=f"{kilometer}km")
+def convert():
+    conversion = round(int(miles_input.get()) * 1.609, 2)
+    km_value.config(text=f"{conversion}")
 
 
-button = Button(text="Convert", command=converter)
-button.grid(column=1, row=1)
+button = Button(text="Calculate", command=convert)
+button.grid(column=1, row=2)
 
-new_botton = Button(text="New Button")
-new_botton.grid(column=2, row=0)
 
 window.mainloop()
-
 
